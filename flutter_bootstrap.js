af93af9d523de8138a6d3a9c36577c95d57ad0a9
Udayteja Cholletti <chollettiudayteja@gmail.com>
2025-04-27 00:58:29 +0530

    push2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "626438430"
+    serviceWorkerVersion: "1250077994"
   }
 });
